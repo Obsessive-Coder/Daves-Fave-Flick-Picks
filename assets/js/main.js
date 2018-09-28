ba76794d1c6b5bd5248dfa4ecb833b2cb84fb0cc
jared huffstutler <jaredhuff85@gmail.com>
2018-09-28 11:52:41 -0700

    Added movie card Added hover overlay

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
 $(() => {
+  $('#movies').on('mouseenter', '.movie', function() {
+    $(this).find('.card-img-overlay').toggleClass('invisible');
+  });
 
+  $('#movies').on('mouseleave', '.movie', function() {
+    $(this).find('.card-img-overlay').toggleClass('invisible');
+  });
+
+  function getMovieHTML(movie) {
+    const movieCard = $('<div class="card movie">');
+
+    const cardImage = $('<img class="card-img">');
+    cardImage.attr('src', movie.imageUrl);
+    cardImage.attr('alt', movie.title);
+
+    const imageOverlay = $('<div class="card-img-overlay invisible">');
+    imageOverlay.append('<h5>' + movie.title + '</h5>');
+    // TODO: Add more html when abstracted from file.
+
+    movieCard.append(cardImage, imageOverlay);
+
+    return movieCard;
+  }
+
+
+
+  const movieCard = getMovieHTML({title: '8 Mile', imageUrl: 'https://via.placeholder.com/200x200'});
+
+  $('#movies').append(movieCard);
 });
