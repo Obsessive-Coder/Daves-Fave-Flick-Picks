6c8d01a95828e1960d6da49b91351dea3e5d6548
jared huffstutler <jaredhuff85@gmail.com>
2018-09-28 13:30:08 -0700

    refactored movie card

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,17 +8,19 @@ $(() => {
     $('#movies').html(movieCards);
   }
 
-  function getMovieHTML(movie) {
+  function getMovieHTML(movie) {;
+
+
     const movieCard = $('<div class="card my-3">');
 
-    const cardBody = $('<div class="card-body d-flex p-0">');
+    const cardBody = $('<div class="card-body row p-0">');
 
-    const cardImage = $('<img class="card-img w-25">');
+    const cardImage = $('<img class="col-5 col-md-4 img-fluid p-0">');
     cardImage.attr('src', movie.imageUrl);
     cardImage.attr('alt', movie.title);
 
-    const cardContent = $('<div class="flex-fill">');
-    cardContent.append('<h4>' + movie.title + '</h4>');
+    const cardContent = $('<div class="col-7 col-md-8">');
+    cardContent.append('<h5>' + movie.title + '</h5>');
 
     // TODO: Add more html when abstracted from file.
 
@@ -29,22 +31,22 @@ $(() => {
 
   const movies = [{
     title: '8 Mile',
-    imageUrl: 'https://via.placeholder.com/200x200'
+    imageUrl: 'https://via.placeholder.com/150x150'
   }, {
     title: 'Fear and Loathing in Las Vegas',
-    imageUrl: 'https://via.placeholder.com/200x200'
+    imageUrl: 'https://via.placeholder.com/150x150'
   }, {
     title: 'Pulp Fiction',
-    imageUrl: 'https://via.placeholder.com/200x200'
+    imageUrl: 'https://via.placeholder.com/150x150'
   }, {
     title: 'I Love You Man',
-    imageUrl: 'https://via.placeholder.com/200x200'
+    imageUrl: 'https://via.placeholder.com/150x150'
   }, {
     title: 'Terminator 2',
-    imageUrl: 'https://via.placeholder.com/200x200'
+    imageUrl: 'https://via.placeholder.com/150x150'
   }, {
     title: 'Kill Bill',
-    imageUrl: 'https://via.placeholder.com/200x200'
+    imageUrl: 'https://via.placeholder.com/150x150'
   }];
 
   showMovies(movies);
